ccfdc17f669d82e7a47ab6b07c7456c3225de7f8
lumen-limitless <lumenlimitless@gmail.com>
2024-08-05 09:15:59 -0400

    new collection opensea link

--- components/nav.tsx
+++ components/nav.tsx
@@ -25,7 +25,7 @@ const social = [
   },
   {
     name: 'opensea',
-    link: 'https://testnets.opensea.io/collection/nft-example-59',
+    link: 'https://testnets.opensea.io/collection/nft-example-60',
     icon: <Icon icon="simple-icons:opensea" />,
   },
   {
